935de12135780683c49cd735d19ab7b9b006e737
kawarimidoll <kawarimidoll+git@gmail.com>
2020-11-09 21:32:08 +0900

    update index.js

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
-module.exports = (options, ctx) => {
-  const name = "@kawarimidoll/vuepress-plugin-tailwind";
-  const { tailwindConfig, purgecssConfig } = options;
-  const { siteConfig, isProd, sourceDir, vuepressDir, cwd } = ctx;
+module.exports = (options = {}, ctx) => {
+  const { name } = require("./package");
+  const { cwd, siteConfig, sourceDir, vuepressDir } = ctx;
+  const { tailwindConfig, ...others } = options;
   const { logger } = require("@vuepress/shared-utils");
 
+  const defaultTailwindConfig = () => {
+    try {
+      return require(`${cwd}/tailwind.config.js`);
+    } catch (e) {
+      const purge = {
+        content: [sourceDir, vuepressDir].map(
+          (dir) => `${dir}/**/*.@(js|ts|md|vue|html)`
+        ),
+      };
+      return Object.assign({ purge }, others);
+    }
+  };
+
   const plugins = [
-    require("tailwindcss")(tailwindConfig),
-    require("autoprefixer")
+    require("tailwindcss")(tailwindConfig || defaultTailwindConfig()),
+    require("autoprefixer"),
   ];
-  logger.tip(`[${name}] tailwindcss is enabled`);
-
-  if (isProd) {
-    const extensions = "@(js|md|vue|html)";
-    plugins.push(
-      require("@fullhuman/postcss-purgecss")(
-        purgecssConfig || {
-          content: [
-            `${sourceDir}/**/*.${extensions}`,
-            `${vuepressDir}/**/*.${extensions}`,
-            `${cwd}/node_modules/**/*vuepress*/!(node_modules)/**/*.${extensions}`
-          ],
-          defaultExtractor: content => content.match(/[\w-\/:]+(?<!:)/g) || []
-        }
-      )
-    );
-    logger.tip(`[${name}] purgecss is enabled`);
-  }
-
   siteConfig.postcss = Object.assign(siteConfig.postcss || {}, { plugins });
 
+  logger.tip(`[${name}] tailwindcss is enabled`);
   return { name };
 };
